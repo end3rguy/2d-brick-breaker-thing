95e695140c33f04ec6fb12fea74aef585f7ad4de
Steve MacLaren <steve@Steves-MacBook-Pro.local>
2021-04-17 11:43:10 -0700

    level1complete

---
+++ Assets/scripts/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle;
+    Vector2 paddleballoffdset;
+    [SerializeField] Vector2 launchvelocity;
+    bool launched = false; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleballoffdset = transform.position - paddle.transform.position;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!launched)
+        {
+            Lockball2paddle();
+            Launchonclick();
+        } 
+    }
+    void Lockball2paddle() {
+       
+
+        Vector2 paddlepos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlepos + paddleballoffdset;
+    }
+    void Launchonclick()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity=launchvelocity;
+            launched = true;
+        }
+    }
+}
---
+++ Assets/scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float minx;
+    [SerializeField] float maxx;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddleposition = new Vector2 (transform.position.x, transform.position.y);
+        float mousexpostion = Input.mousePosition.x;
+        //Debug.Log(mousexpostion/Screen.width*16f);
+        paddleposition.x = Mathf.Clamp(mousexpostion / Screen.width * 16f, minx, maxx);
+        transform.position = paddleposition;
+
+    }
+}
