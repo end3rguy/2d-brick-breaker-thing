924cef5932fd366505748c483aeddd18c7dfc4f1
end3rguy <end3rguy@gmail.com>
2021-04-24 12:00:24 -0700

    added a function so that once all bricks are destroyed the next level will start

--- Assets/scripts/Brick.cs
+++ Assets/scripts/Brick.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
+    Level level;
     // Start is called before the first frame update
     void Start()
     {
-        
+        level = FindObjectOfType<Level>();
+        level.Countbrix();
     }
 
     // Update is called once per frame
@@ -20,6 +22,7 @@ public class Brick : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log(collision);
+        level.Removebrix();
         Destroy(gameObject);
     }
 }
---
+++ Assets/scripts/Level.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int numbrix = 0;
+     SceneLoader sceneloader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();
+    }
+    public void Countbrix() {
+
+        numbrix = numbrix + 1;
+
+    }
+    public void Removebrix() {
+
+        numbrix = numbrix - 1;
+
+        if (numbrix == 0) {
+
+            sceneloader.LoadNextScene();
+
+        }
+
+
+    }
+}
